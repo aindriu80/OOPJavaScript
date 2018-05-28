d8faa580d17af860898bf0d44d90ca27f9a67d30
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-28 15:29:42 +0100

    Factories in JavaScript

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
-// Object literals
-
-// Object literal syntax
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function () {
-        console.log('draw');
-    }
-};
-
+// Factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
+    };
+}
+const circle = createCircle(1);
 circle.draw();
