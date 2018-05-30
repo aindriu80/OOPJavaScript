b86ad07f6fafae7efa71afa5f14e2d2bc43839b3
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-30 15:44:51 +0100

    Abstraction

--- js/index.js
+++ js/index.js
@@ -10,11 +10,10 @@ let b = a;
 
 a = 20;
 
-
 // Reference type, reference is copied
 // x = 20, y = 20
 let x = {
-    value: 10
+  value: 10
 };
 let y = x;
 
@@ -23,54 +22,34 @@ x.value = 20;
 // Primitives are copied by their value
 // Objects are copied by their reference
 
-
-
 let obj = {
-    value: 10
+  value: 10
 };
 
 function increase(obj) {
-    obj.value++;
+  obj.value++;
 }
 
 // object is passed by its reference
 increase(obj);
 console.log(obj);
 
-// Adding or Removing Properties
+// Abstraction, hiding the details, showing the essentials
 
 function Circle(radius) {
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
+  this.radius = radius;
+
+  this.defaultLocation = { x: 0, y: 0 };
+
+  this.computeOptimLocation = function() {
+    // ...
+  };
+  this.draw = function() {
+    this.computeOptimLocation();
+    console.log("draw");
+  };
 }
 
 const circle = new Circle(10);
-
-circle.location = {
-    x: 1
-};
-
-const propertyName = 'center location';
-circle.location = {
-    x: 1
-};
-
-delete circle['location'];
-
-// To enemurate all members use a for loop
-for (let key in circle) {
-    // use typeof operator to check type of value
-    if (typeof circle[key] !== 'function')
-        console.log(key, circle[key]);
-}
-
-// To get all the keys use Object.keys
-const keys = Object.keys(circle);
-console.log(keys);
-
-// To check of the existence of a property 
-// use the in operator.
-if ('radius' in circle)
-    console.log('Circle has a radius.');
+circle.computeOptimLocation();
+circle.draw();
