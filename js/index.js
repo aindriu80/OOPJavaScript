c8ffdc8a58e6f1a5d82b891875aec591d811258a
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-02 16:34:05 +0100

    Mixins

--- js/index.js
+++ js/index.js
@@ -4,14 +4,13 @@ function Shape(color) {
   this.color = color;
 }
 
-Shape.prototype.duplicate = function () {
-  console.log('duplicate');
-}
+Shape.prototype.duplicate = function() {
+  console.log("duplicate");
+};
 
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Circle;
-
 }
 
 function Circle(radius, color) {
@@ -22,16 +21,15 @@ function Circle(radius, color) {
 
 extend(Circle, Shape);
 
-Circle.prototype.duplicate = function () {
+Circle.prototype.duplicate = function() {
   Shape.prototype.duplicate.call(this);
 
-  console.log('duplicate circle');
-}
+  console.log("duplicate circle");
+};
 
-Circle.prototype.draw = function () {
-  console.log('draw');
-}
-
+Circle.prototype.draw = function() {
+  console.log("draw");
+};
 
 function Square(size) {
   this.size = size;
@@ -40,34 +38,65 @@ function Square(size) {
 extend(Square, Shape);
 
 const s = new Shape();
-const c = new Circle(1, 'red');
+const c = new Circle(1, "red");
 
 // Polymorphism
 
-function Circle1() {
-
-}
+function Circle1() {}
 
 extend(Circle1, Shape);
 
-Circle.prototype.duplicate = function () {
-  console.log('duplicate circle');
-}
+Circle.prototype.duplicate = function() {
+  console.log("duplicate circle");
+};
 
-function Square() {
-
-}
+function Square() {}
 
 extend(Square, Shape);
 
-Square.prototype.duplicate = function () {
-  console.log('duplicate square');
+Square.prototype.duplicate = function() {
+  console.log("duplicate square");
+};
+
+const shapes = [new Circle(), new Square()];
+
+for (let shape of shapes) shape.duplicate();
+
+// Mixins
+function mixin(target, ...sources) {
+  Object.assign(target, ...sources);
 }
+const canEat = {
+  eat: function() {
+    this.hunger--;
+    console.log("eating");
+  }
+};
 
-const shapes = [
-  new Circle(),
-  new Square()
-];
+const canWalk = {
+  walk: function() {
+    console.log("walking");
+  }
+};
 
-for (let shape of shapes)
-  shape.duplicate();
+const canSwim = {
+  swim: function() {
+    console.log("swim");
+  }
+};
+
+function Person() {}
+
+// Object.assign(Person.prototype, canEat, canWalk);
+mixin(Person.prototype, canEat, canWalk);
+
+const person = new Person();
+console.log(person);
+
+function Goldfish() {}
+
+// Object.assign(Goldfish.prototype, canEat, canSwim);
+mixin(Goldfish.prototype, canEat, canWalk);
+
+const goldfish = new Goldfish();
+console.log(goldfish);
