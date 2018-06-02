bf851efe2acf8d05f28fbac4a96a415749a5c023
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-02 10:11:16 +0100

    Creating own Prototypical inheritance

--- js/index.js
+++ js/index.js
@@ -1,65 +1,22 @@
-// Value vs Reference Types.
+// Creating own Prototypical inheritence
 
-// Value type
-// using let instead of var
-let a = 10;
-let b = a;
+function Shape() {}
 
-// Two independent variables
-// a is 20, b is 10
-
-a = 20;
-
-// Reference type, reference is copied
-// x = 20, y = 20
-let x = {
-  value: 10
-};
-let y = x;
-
-x.value = 20;
-
-// Primitives are copied by their value
-// Objects are copied by their reference
-
-let obj = {
-  value: 10
-};
-
-function increase(obj) {
-  obj.value++;
+Shape.prototype.duplicate = function () {
+  console.log('duplicate');
 }
-
-// object is passed by its reference
-increase(obj);
-console.log(obj);
-
-// Abstraction, hiding the details, showing the essentials
 
 function Circle(radius) {
   this.radius = radius;
-
-  let defaultLocation = { x: 0, y: 0 };
-
-  this.getDefaultLocation = function() {
-    return defaultLocation;
-  };
-  this.draw = function() {
-    console.log("draw");
-  };
-
-  Object.defineProperty(this, "defaultLocation", {
-    get: function() {
-      return defaultLocation;
-    },
-    set: function(value) {
-      if (!value.x || !value.y) throw new Error("Invalid location.");
-
-      defaultLocation = value;
-    }
-  });
 }
 
-const circle = new Circle(10);
-circle.defaultLocation = 1;
-circle.draw();
+// Circle.prototype = Object.create(Object.prototype); // objectBase
+Circle.prototype = Object.create(Shape.prototype);
+
+
+Circle.prototype.draw = function () {
+  console.log('draw');
+}
+
+const s = new Shape();
+const c = new Circle(1);
